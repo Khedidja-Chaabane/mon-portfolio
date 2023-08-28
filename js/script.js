50ad7e682c85cc847aa91ca05f9af4208abc4973
kadija-larbi <khedidjal94@gmail.com>
2023-08-28 23:05:44 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Sélectionner l'élément vidéo
+const video = document.getElementById("myVideo");
+
+// Ralentir la lecture de la vidéo (par exemple, 0.5 est la moitié de la vitesse normale)
+video.playbackRate = 0.25;
